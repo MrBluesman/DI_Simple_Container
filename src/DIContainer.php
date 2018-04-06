dd1bb41f4d06387b7188b3a2e6b414e9fbc057c2
Łukasz <l.parysek@gmail.com>
2018-04-06 23:45:47 +0200

    Dependency get from container

--- src/DIContainer.php
+++ src/DIContainer.php
@@ -18,7 +18,7 @@ class DIContainer
 
     /**
      * Registers on dependency in the container
-     * Creates an array with @value and optional @args, matches with the @key end stores
+     * Creates an array with $value and optional $args, matches with the $key end stores
      * @param $key string identifier of the dependency in the map
      * @param $value object or primitive type to store in dependencies
      * @param $args array optional constructor arguments
@@ -33,7 +33,7 @@ class DIContainer
 
     /**
      * Helping method which only adds a dependency of object to the storage map
-     * @param $key string identifier of the dependency in the map
+     * @param $key string identifier    of the dependency in the map
      * @param $object object which dependency will be store in the @DIContainer
      */
     private function addDependency($key, $object)
@@ -41,5 +41,64 @@ class DIContainer
         //create a new array if depStorage is Empty
         if($this->depStorage === NULL) $this->depStorage = (object) array();
         $this->depStorage->$key = $object;
+    }
+
+    /**
+     * Gets an instance of stored dependency
+     * @param $key string identifier if the dependency in the map
+     * @return object dependency identified by @key
+     * @throws ReflectionException
+     * @throws Exception
+     */
+    public function get($key)
+    {
+        //Check if the key exist
+        if(!array_key_exists($key, $this->depStorage))
+        {
+            throw new Exception("DIContainer: '".$key."' doesn't exist in the container.");
+        }
+        else
+        {
+            //get inform from depStorage array
+            $className = $this->depStorage->$key->value;
+            $args = $this->depStorage->$key->args;
+
+            //only if class exists we can make a instance of ReflectionClass to return
+            if(class_exists($className))
+            {
+                $reflection = new ReflectionClass($className);
+
+                //class with args or without args
+                if($args === NULL || count($args) == 0)
+                {
+                    $objectToReturn = new $className;
+                }
+                else
+                {
+                    if(!is_array($args)) $args = array($args);
+                    $objectToReturn = $reflection->newInstanceArgs($args);
+                }
+            }
+            else $objectToReturn = $this->depStorage->$key->value;
+        }
+        return $objectToReturn;
+    }
+
+    /**
+     * depStorage getter
+     * @return array of dependencies
+     */
+    public function getDepStorage()
+    {
+        return $this->depStorage;
+    }
+
+    /**
+     * depStorage setter
+     * @param array $depStorage new dependencies to replace with previous
+     */
+    public function setDepStorage($depStorage)
+    {
+        $this->depStorage = $depStorage;
     }
 }
